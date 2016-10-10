fdf136d6f424d68c5d0eaa0f03d374d5ef289638
srinivas  Vaidyanathan <srinivas@srinivass-MacBook-Pro.local>
2016-10-10 14:52:05 +0530

    added test for the plot production

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
-from main import current
+from main import current,makePlot
 import unittest as ut
 import numpy as np
+import os
 class TestCurrent(ut.TestCase):
 	def setUp(self):
 		print("setUp") 
@@ -12,6 +13,9 @@ class TestCurrent(ut.TestCase):
 		self.assertEqual(current(1,1,12,np.linspace(0,2,num=10),0.5)[0],2.0)
 	def test_checkOverDamped(self):
 		self.assertEqual(current(1,1,12,np.linspace(0,2,num=10),1.5)[0],2.0)
-
+	def test_checkPlotting(self):
+		makePlot(1,1,12,np.linspace(0,2,num=10),[1.5])
+		self.assertTrue(os.path.isfile('./plots.png'))
+		os.remove('./plots.png')
 if __name__=='__main__':
 	ut.main()
