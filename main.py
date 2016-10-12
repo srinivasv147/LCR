499b363cfafb9b1d8c2c1e461796416e76bd2186
srinivas  Vaidyanathan <srinivas@srinivass-MacBook-Pro.local>
2016-10-12 14:36:54 +0530

    added code to beautify the graphs

--- main.py
+++ main.py
@@ -16,15 +16,34 @@ def current(A,B,w,t,eta):
 		return
 
 def makePlot(A,B,w,t,eta,s):
+	fig=plt.figure()
+	fig.suptitle('current vs time', fontsize=14,fontweight='bold',family='serif')
+	minTitle="eta="
+	eta=eta[:3]#we only plot the first 3 etas for well defined plots.
+	color=['red','blue','green']
+	markers=['o','^','*']
+	plts=[]
+	etastr=[]
+	i=0
 	for e in eta:
 		currentVal=current(A,B,w,t,e)
-		plt.plot(t,currentVal)
+		newPlot,=plt.plot(t,currentVal,color=color[i],marker=markers[i],linewidth=3.3)
+		minTitle+=str(e)+','
+		plts.append(newPlot)
+		etastr.append("eta="+str(e))
+		i+=1
+	plt.legend(plts,etastr,fontsize=14)
+	ax = fig.add_subplot(111)
+	ax.set_title(minTitle[:-1])
+	ax.set_xlabel('time(sec)',fontsize=14,style='italic',family='serif')
+	ax.set_ylabel('current(amp)',fontsize=14,style='italic',family='serif')
+	# plt.show()
 	plt.savefig(s+'.png')
 
 if __name__=='__main__':
 	A=1
 	B=1#we assume the constants to be 1
 	w=12#the value of natural frequency which we consider
-	t=np.linspace(0,10,num=1000)#the range of time values
-	eta=[0.5,1,1.5]
+	t=np.linspace(0,1,num=100)#the range of time values
+	eta=[0.5,1,1.5,2.5]
 	makePlot(A,B,w,t,eta,'plots')
